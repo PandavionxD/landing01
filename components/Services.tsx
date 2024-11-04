a2f6d4baa50177af028f56e1b83b9c6f3988ded1
Alex Daniel <1151372@senati.pe>
2024-11-03 21:03:29 -0500

    Terminando seccion Testimonial

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,33 +1,32 @@
-import { dataServices } from "@/data"
-import { Title } from "./Shared/Title"
-import { Check } from "lucide-react"
+import { dataServices } from "@/data";
+import { Title } from "./Shared/Title";
+import { Check } from "lucide-react";
 
 export const Services = () => {
   return (
-    <div className="p-6 md:px-12 md:py-24 max-w-5xl"  id="services">
-        <Title  title="Servicios" subtitle="Que ofrezco" />
-        <div className="grid md:grid-cols-3 gap-5 mt-7" >
-            {
-                dataServices.map(service=>(
-                    <div key={service.id} className="rounded-xl border-slate-400 border-2 p-6 dark:bg-slate-800" >
-                        <h4 className="mb-4 text-xl flex gap-2" >
-                        {service.icon}
-                        {service.title}
-                        </h4>
-                        <ul>
-                            {
-                                service.features.map((feature,id)=>(
-                                    <li key={id} className="flex gap-3 mb-3" >
-                                        <Check size={28} />
-                                        {feature.name}
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </div>
-                ))
-            }
-        </div>
+    <div className="p-6 md:px-12 md:py-24 max-w-5xl mx-auto" id="services">
+      <Title title="Servicios" subtitle="Que ofrezco" />
+      <div className="grid md:grid-cols-3 gap-5 mt-7">
+        {dataServices.map((service) => (
+          <div
+            key={service.id}
+            className="rounded-xl border-slate-400 border-2 p-6 dark:bg-slate-800"
+          >
+            <h4 className="mb-4 text-xl flex gap-2">
+              {service.icon}
+              {service.title}
+            </h4>
+            <ul>
+              {service.features.map((feature, id) => (
+                <li key={id} className="flex gap-3 mb-3">
+                  <Check size={28} />
+                  {feature.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -1,6 +1,42 @@
+import { dataTestimonials } from "@/data";
+import { Title } from "./Shared/Title";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export const Testimonial = () => {
   return (
-    <div>Testimonial</div>
-  )
-}
+    <div className="p-4 md:px-12 md:py-44 max-w-5xl mx-auto ">
+      <Title title="Testimonios" subtitle="Review de los clientes" />
+      <Carousel className="mx-10 mt-6">
+        <CarouselContent className="gap-8">
+          {dataTestimonials.map((testimonial) => (
+            <CarouselItem
+              key={testimonial.id}
+              className="md:basis-1/3 p-4 flex flex-col items-center border-slate-400 border-2 rounded-lg"
+            >
+              <Avatar>
+                <AvatarImage src={testimonial.imageUrl} alt="Profile-avatar" />
+
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div>
+                <p className="min-h-12 text-2xl mt-4 text-center">
+                  {testimonial.name}
+                </p>
+                <p>{testimonial.description}</p>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
